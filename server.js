b0a4415413ff6cae9b490e95fd77e7d1fcf0d3f4
crowntj07 <66528327+crowntj07@users.noreply.github.com>
2020-10-26 20:26:23 -0500

    updated line 15

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static('./'));
-jb 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
