2eea29331c3d06ea8dcb1083196d135f4a62d378
Todd Jones <jonesgtodd@gmail.com>
2020-10-26 20:17:45 -0500

    updated api and html routes

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+
+const PORT = process.env.PORT || 8001;
+
+
+const dbJson = require('./db/db.json')
+
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
+jb 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
